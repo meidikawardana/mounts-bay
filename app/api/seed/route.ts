38e630b39aca955ceaec9acc9d8053e784362430
Meidika Wardana <jdeep555.mw@gmail.com>
2025-01-13 18:21:04 +0700

    Fix 405 method allowed returned by /api/seed

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,11 +1,21 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 import { main } from '../../../prisma/seed'
 
 const prisma = new PrismaClient()
 
-export async function POST() {
+// Add export config to explicitly allow POST
+export const config = {
+    runtime: 'edge',
+    regions: ['all'],
+}
+
+// Add dynamic flag to ensure route handler is recognized
+export const dynamic = 'force-dynamic'
+
+export async function POST(req: NextRequest) {
     try {
+        console.info('request: ' + req)
         await main()
 
         return NextResponse.json({ message: "Database seeded successfully" })
